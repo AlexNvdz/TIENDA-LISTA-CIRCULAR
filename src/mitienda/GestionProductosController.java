ba349feb65af46a327255b67d047bf0fd3354ab6
Alex Nvdz <Alex Nvdz@192.168.1.52>
2023-10-27 08:25:19 -0500

    IMPLEMENTACION GUI INCOMPLETA

--- src/mitienda/GestionProductosController.java
+++ src/mitienda/GestionProductosController.java
@@ -1,25 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
 package mitienda;
+
 import java.net.URL;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javax.swing.JOptionPane;
 
-public class GestionProductosController {
-    
-    String idProducto="", nomProducto="";
-    LocalDate fechaLote = null, fechaVence=null;
+/**
+ *
+ * @author Alex Nvdz
+ */
+public class GestionProductosController implements Initializable {
+
+    String idProducto = "", nomProducto = "";
+    LocalDate fechaLote = null, fechaVence = null;
     float precioU = -1;
     PStack pilaProductos = new PStack();
 
@@ -39,7 +51,7 @@ public class GestionProductosController {
     private MenuBar MenuBar;
 
     @FXML
-    private TableView<?> MiTabla;
+    private TableView<Producto> MiTabla;
 
     @FXML
     private MenuItem Op1;
@@ -88,56 +100,72 @@ public class GestionProductosController {
 
     @FXML
     private TextField txtPrecio;
-    
+
+    private ObservableList<Producto> ListaProductos;
+    @FXML
+    private TableColumn<Producto, String> idColumn;
+    @FXML
+    private TableColumn<Producto, String> nomColumn;
+    @FXML
+    private TableColumn<Producto, String> fechaLColumn;
+    @FXML
+    private TableColumn<Producto, String> fechaVColumn;
+    @FXML
+    private TableColumn<Producto, Integer> precioUColumn;
+    @FXML
+    private ComboBox<String> BoxMeses;
+
     @FXML
     void eventoSalir(ActionEvent event) {
         System.exit(0);
     }
-    
-    
-     @FXML
+
+    @FXML
     void EventoMostrar(ActionEvent event) {
         pilaProductos.mostrarProductos();
     }
-    
-    private ObservableList<Producto> listaProductos = FXCollections.observableArrayList();
-    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("idProducto"));
+        nomColumn.setCellValueFactory(new PropertyValueFactory<>("nomProducto"));
+        fechaLColumn.setCellValueFactory(new PropertyValueFactory<>("fechaLote"));
+        fechaVColumn.setCellValueFactory(new PropertyValueFactory<>("fechaVence"));
+        precioUColumn.setCellValueFactory(new PropertyValueFactory<>("precioU"));
+
+        // Inicializa la lista observable de productos
+        ListaProductos = FXCollections.observableArrayList();
+
+        // Asocia la lista de productos a la tabla
+        MiTabla.setItems(ListaProductos);
+
+        ObservableList<String> opciones = FXCollections.observableArrayList(
+                "idProducto", "nomProducto", "FechaLote", "FechaVence", "precioU");
+    }
+
     @FXML
-    void eventoRegitrar(ActionEvent event) throws ParseException {
+    void eventoRegitrar(ActionEvent event) {
         String id = txtID.getText();
         String nom = txtNombre.getText();
-        if (getFechaL.getValue()==null && getFechaV.getValue()==null) {
+        if (getFechaL.getValue() == null && getFechaV.getValue() == null) {
             JOptionPane.showMessageDialog(null, "Ingrese las fechas!!!");
             return;
         } else {
-           
-                 try {
-        
-                 LocalDate fechaLote = getFechaL.getValue();
-                 LocalDate fechaVence = getFechaV.getValue() ;
-                 float precio = Float.parseFloat(txtPrecio.getText());
 
-                Producto producto = new Producto(id, nom, fechaLote, fechaVence, precio);
-                pilaProductos.setPushProducto(producto);
-                
-                 txtID.clear();
-                 txtNombre.clear();
-                 getFechaL.setValue(null);
-                 getFechaV.setValue(null);
-                 txtPrecio.clear();
-                
-                } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Error al procesar fechas o precio.");
-                }
-            }
+            LocalDate fechaLote = getFechaL.getValue();
+            LocalDate fechaVence = getFechaV.getValue();
+            float precio = Float.parseFloat(txtPrecio.getText());
+
+            Producto producto = new Producto(id, nom, fechaLote, fechaVence, precio);
+            pilaProductos.setPushProducto(producto);
+            ListaProductos.add(producto);
+            txtID.clear();
+            txtNombre.clear();
+            getFechaL.setValue(null);
+            getFechaV.setValue(null);
+            txtPrecio.clear();
+            txtID.requestFocus();
         }
-  
-     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-         
-
-     }
     }
-    
-    
-
+}
